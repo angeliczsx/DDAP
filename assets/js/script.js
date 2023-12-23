7a4985806079a8864797b0b936244cc3d6ba8c01
Ananda Ravi Kuntadi <118543324+Keiinz@users.noreply.github.com>
2023-12-23 23:48:29 +0700

    fixing bug LASTTT

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('NavMobile').addEventListener('click', function () {
-    const menu = document.getElementById('MenuItems');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
